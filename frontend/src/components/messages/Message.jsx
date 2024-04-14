4274d86ad6a8c7a2495190d25675c1e83f5f1f3c
Vikash Sangai <vikash36905@gmail.com>
2024-04-14 16:30:33 +0530

    app ui done

---
+++ frontend/src/components/messages/Message.jsx
@@ -0,0 +1,15 @@
+const Message = () => {
+  return (
+    <div className="chat chat-end">
+        <div className="chat-image avatar">
+            <div className="w-10 rounded-full">
+                <img src="" alt="Tailwind css chat bubble component" />
+            </div>
+        </div>
+        <div className="chat-bubble text-white bg-blue-500">Hi!</div>
+        <div className="chat-footer opacity-50 text-xs flex gap-1 items-center">15:57</div>
+    </div>
+  )
+}
+
+export default Message
